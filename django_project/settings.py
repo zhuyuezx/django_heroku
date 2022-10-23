93f5bb282501e6d22540077136c0414878136d43
zhuyuezx <zhuyuezx@gmail.com>
2022-10-23 04:04:09 -0400

    signup module update

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'blog.apps.BlogConfig',
     'accounts.apps.AccountsConfig',
 ]
